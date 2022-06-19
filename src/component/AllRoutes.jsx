2cd9caafb19098c3da39f22bf63d892f9e8b5b5c
sacgin ingole <97524423+sachin-rock@users.noreply.github.com>
2022-06-19 12:27:12 +0530

    done
    done

---
+++ src/component/AllRoutes.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import CreateCalendar from "../pages/CreateCalendar";
+import Pricing from "../pages/Pricing";
+import Home from "../component/Home";
+// import Signin from "../pages/Signin";
+// import Signup from "../pages/Signup";
+// import GetADemo from "../pages/GetADemo";
+// import Actionable from "../pages/Actionable_Marketing/Actionable";
+// import Customer from "../pages/Customer_Stories/Customer";
+// import GroceryItem from "../pages/mktcal";
+import Signin from "../pages/Signin";
+import Signup from "../pages/Signup";
+import GetADemo from "../pages/GetADemo";
+import Actionable from "../pages/Actionable_Marketing/Actionable";
+import Customer from "../pages/Customer_Stories/Customer";
+import GroceryItem from "../pages/mktcal";
+
+const AllRoutes = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Calender" element={<CreateCalendar />} />
+        {/* <Route path="/Pricing" element={<Pricing />} />
+        <Route path="/Signin" element={<Signin />} />
+        <Route path="/Signup" element={<Signup />} />
+        <Route path="/Actionable" element={<Actionable />} />
+        <Route path="/demo" element={<GetADemo />} /> */}
+        <Route path="/Pricing" element={<Pricing />} />
+        <Route path="/Signin" element={<Signin />} />
+        <Route path="/Signup" element={<Signup />} />
+        <Route path="/Actionable" element={<Actionable />} />
+        <Route path="/demo" element={<GetADemo />} />
+        <Route path="/Customer" element={<Customer />} />
+        <Route path="/mtkcal" element={<GroceryItem />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default AllRoutes;
